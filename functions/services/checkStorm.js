fad1d24cec7b0ca9132678586ca804e067aa0f1e
lob23 <hvblong21@apcs.fitus.edu.vn>
2024-10-16 17:06:01 +0700

    change api url

--- functions/services/checkStorm.js
+++ functions/services/checkStorm.js
@@ -6,7 +6,7 @@ require('dotenv').config(); // Load environment variables
 const getStormInfo = async (latitude, longitude) => {
     const apiKey = process.env.OPENWEATHER_API_KEY; // Replace with your actual OpenWeatherMap API key
     // console.log("API KEY: ", apiKey)
-    const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,daily&appid=${apiKey}`;
+    const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly&appid=${apiKey}`;
 
     try {
         const response = await axios.get(apiUrl);
