56231fc4641de1fec28e477d5b8d56584a5993a5
Văn Phúc Lưu <hoktro18@gmail.com>
2024-10-24 03:35:11 +0700

    fix: error response has no describe msg

--- functions/routes/route/userRoutes.js
+++ functions/routes/route/userRoutes.js
@@ -10,20 +10,17 @@ const UserAccount = require("@models/class/userAccount");
 */
 // Create a new user
 router.post("/create", async (req, res) => {
-  console.log(req.body);
-  (async () => {
-    try {
-      const newUser = await UserAccount.create(req.body);
-      return res.status(200).send({
-        success: "Success",
-        message: "User created successfully",
-        userId: newUser.UserId,
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send({success: "Failed", message: error});
-    }
-  })();
+  try {
+    const newUser = await UserAccount.create(req.body);
+    return res.status(200).send({
+      success: "Success",
+      message: "User created successfully",
+      userId: newUser.UserId,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({success: "Failed", message: error});
+  }
 });
 
 /*
@@ -33,29 +30,24 @@ router.post("/create", async (req, res) => {
  */
 // Get single user by userID
 router.get("/get/:userId", async (req, res) => {
-  (async () => {
-    try {
-      const userAccount = await UserAccount.getById(req.params.userId);
-      return res.status(200).send({status: "Success", data: userAccount});
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send({success: "Failed", message: error});
-    }
-  })();
+  try {
+    const userAccount = await UserAccount.getById(req.params.userId);
+    return res.status(200).send({status: "Success", data: userAccount});
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({success: "Failed", message: error.message});
+  }
 });
 
 // Get all users
 router.get("/getAll", async (req, res) => {
-  console.log("Get all users");
-  (async () => {
-    try {
-      const users = await UserAccount.getAll();
-      return res.status(200).send({status: "Success", data: users});
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send({success: "Failed", message: error});
-    }
-  })();
+  try {
+    const users = await UserAccount.getAll();
+    return res.status(200).send({status: "Success", data: users});
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({success: "Failed", message: error});
+  }
 });
 
 /*
@@ -65,28 +57,24 @@ router.get("/getAll", async (req, res) => {
 */
 // Update single user by userID
 router.put("/update/:userId", async (req, res) => {
-  (async () => {
-    try {
-      await UserAccount.update(req.params.userId, req.body);
-      return res.status(200).send({success: "Success", message: "User updated successfully"});
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send({success: "Failed", message: error});
-    }
-  })();
+  try {
+    await UserAccount.update(req.params.userId, req.body);
+    return res.status(200).send({success: "Success", message: "User updated successfully"});
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({success: "Failed", message: error});
+  }
 });
 
 // Update location of single user by userID
 router.put("/updateLocation/:userId", async (req, res) => {
-  (async () => {
-    try {
-      await UserAccount.updateLocation(req.params.userId, req.body);
-      return res.status(200).send({success: "Success", message: "User location updated successfully"});
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send({success: "Failed", message: error});
-    }
-  })();
+  try {
+    await UserAccount.updateLocation(req.params.userId, req.body);
+    return res.status(200).send({success: "Success", message: "User location updated successfully"});
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({success: "Failed", message: error});
+  }
 });
 
 /*
@@ -96,15 +84,13 @@ router.put("/updateLocation/:userId", async (req, res) => {
 */
 // Delete single user by userID
 router.delete("/delete/:userId", async (req, res) => {
-  (async () => {
-    try {
-      await UserAccount.delete(req.params.userId);
-      return res.status(200).send({success: "Success", message: "User deleted successfully"});
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send({success: "Failed", message: error});
-    }
-  })();
+  try {
+    await UserAccount.delete(req.params.userId);
+    return res.status(200).send({success: "Success", message: "User deleted successfully"});
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({success: "Failed", message: error});
+  }
 });
 
 module.exports = router;
